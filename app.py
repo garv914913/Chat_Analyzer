c99cdc34d32661d44bc2f918bf37d9e1d857e7d3
garv914913 <garv914913@gmail.com>
2025-11-21 22:11:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,147 @@
+import streamlit as st
+import preprocessor,helper
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from wordcloud import WordCloud
+
+# from helper import most_common_words
+
+st.sidebar.title("Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        data = bytes_data.decode("utf-8")
+        df = preprocessor.preprocess(data)
+
+        #Fetch unique users
+        user_list = df['users'].unique().tolist()
+        user_list.remove('group_notification')
+        user_list.sort()
+        user_list.insert(0,'Overall')
+
+        selected_user = st.sidebar.selectbox("Show Analysis wrt", user_list)
+
+        # ----------- DATE FILTER ------------
+        st.sidebar.subheader("Filter by Date Range")
+
+        start_date = st.sidebar.date_input("From Date", df['only_date'].min())
+        end_date = st.sidebar.date_input("To Date", df['only_date'].max())
+
+        start_date = pd.to_datetime(start_date)
+        end_date = pd.to_datetime(end_date)
+
+        # Apply filtering
+        df = df[(df['only_date'] >= start_date) & (df['only_date'] <= end_date)]
+        # -------------------------------------
+
+        if st.sidebar.button("Show Analysis"):
+
+            #Stats Area
+            num_messages, words, num_media, links = helper.fetch_stats(selected_user,df)
+            st.title("Top Statistics")
+            col1, col2, col3, col4 = st.columns([1, 1, 1, 1], gap = "large")
+
+            with col1:
+                st.header("Total Messages")
+                st.title(num_messages)
+            with col2:
+                st.header("Total Words")
+                st.title(words)
+            with col3:
+                st.header("Media Shared")
+                st.title(num_media)
+            with col4:
+                st.header("Link Shared")
+                st.title(links)
+
+            #Monthly timeline
+            st.title("Monthly Timeline")
+            timeline = helper.monthly_timeline(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.plot(timeline['time'], timeline['messages'], color = 'red')
+            plt.xticks(rotation = 90)
+            st.pyplot(fig)
+
+            #Daily Timeline
+            st.title("Daily Timeline")
+            daily_timeline = helper.daily_timeline(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.plot(daily_timeline['only_date'], daily_timeline['messages'], color='black')
+            plt.xticks(rotation = 90)
+            st.pyplot(fig)
+
+            #Activity Map
+            st.title('Activity Map')
+
+            col1, col2 = st.columns([1,1], gap = "large")
+
+            with col1:
+                st.header("Most busy day")
+                busy_day = helper.week_activity_map(selected_user, df)
+                fig, ax = plt.subplots()
+                ax.bar(busy_day.index, busy_day.values)
+                plt.xticks(rotation = 90)
+                st.pyplot(fig)
+
+            with col2:
+                st.header("Most busy month")
+                busy_month = helper.month_activity_map(selected_user, df)
+                fig, ax = plt.subplots()
+                ax.bar(busy_month.index, busy_month.values, color = 'orange')
+                plt.xticks(rotation = 90)
+                st.pyplot(fig)
+
+            st.title("Weekly Activity Map")
+            my_heatmap = helper.activity_heatmap(selected_user, df)
+            fig, ax = plt.subplots()
+            ax = sns.heatmap(my_heatmap)
+            st.pyplot(fig)
+
+
+            #finding the busiest users in the group(Group Level)
+            if selected_user == 'Overall':
+                st.title("Most Busy Users")
+                x, new_df = helper.most_busy_user(df)
+                fig, ax = plt.subplots()
+
+                col1, col2 = st.columns([1,1], gap = "large")
+
+                with col1:
+                    ax.bar(x.index, x.values)
+                    plt.xticks(rotation = 90)
+                    st.pyplot(fig)
+                with col2:
+                    st.dataframe(new_df)
+
+            #WordCloud
+            st.title("Wordcloud")
+            df_wc = helper.create_wordcloud(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.imshow(df_wc)
+            st.pyplot(fig)
+
+            #Most Common Words
+            st.title("Most Common words")
+            most_common_df = helper.most_common_words(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.barh(most_common_df[0],most_common_df[1], color = 'green')
+            # plt.xticks(rotation = "vertical")
+            st.pyplot(fig)
+            # st.dataframe(most_common_df)
+
+            #Emoji Analysis
+            emoji_df = helper.emoji_analysis(selected_user, df)
+            st.title("Emoji Analysis")
+
+            col1, col2 = st.columns([1,1], gap = "large")
+
+            with col1:
+                st.dataframe(emoji_df)
+
+            with col2:
+                plt.rcParams['font.family'] = 'DejaVu Sans'
+                fig, ax = plt.subplots()
+                ax.pie(emoji_df['count'].head(), labels = emoji_df['emoji'].head(), autopct="%0.2f", textprops = {'fontsize': 18})
+                st.pyplot(fig)
---
+++ preprocessor.py
@@ -0,0 +1,67 @@
+import re
+import pandas as pd
+from datetime import datetime
+
+def preprocess(data):
+    pattern = r'\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\s(?:AM|PM)\s-\s'
+
+    message = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_message': message, 'message_date': dates})
+    #convert message data type
+    df['message_date'] = pd.to_datetime(df['message_date']
+        .str.replace('\u202f', ' ', regex=False)  # replace narrow non-breaking space
+        .str.replace('\u00a0', ' ', regex=False),  # replace normal non-breaking space
+        format='%m/%d/%y, %H:%M %p - ')
+
+    df.rename(columns = {'message_date': 'date'}, inplace = True)
+
+    #Seperate Users and Messages
+    users = []
+    messages = []
+    for message in df['user_message']:
+        if not isinstance(message, str):
+            message = str(message)
+        entry = re.split(r'([\w\W]+?):\s', message)
+        if entry[1:]:
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['users'] = users
+    df['messages'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['only_date'] = df['date'].dt.date
+    df['only_date'] = pd.to_datetime(df['only_date'])
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['hour_label'] = df['date'].dt.strftime('%I %p')
+    df['minute'] = df['date'].dt.minute
+
+    period = []
+    for (hour_label, hour) in zip(df['hour_label'], df['hour']):
+        if hour_label == '11 PM':
+            period.append(str(hour_label) + "-" + str('00 AM'))
+        elif hour_label == '11 AM':
+            period.append(str(hour_label) + "-" + str('12 PM'))
+        elif hour_label == '12 PM':
+            period.append(str(hour_label) + "-" + str('1 PM'))
+        elif hour_label == '00 AM':
+            period.append(str(hour_label) + "-" + str('1 AM'))
+        else:
+            start = hour_label
+            end = datetime.strptime(str((hour + 1) % 24), "%H").strftime("%I %p")
+            period.append(f"{start}-{end}")
+
+    df['period']= period
+
+    return df
