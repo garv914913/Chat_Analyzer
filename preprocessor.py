2236d490d8d4a670eca7cf7a628cc0afde9edf9f
garv914913 <garv914913@gmail.com>
2025-12-18 22:33:27 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -39,8 +39,8 @@ def preprocess(data):
     df['year'] = df['date'].dt.year
     df['month_num'] = df['date'].dt.month
     df['month'] = df['date'].dt.month_name()
-    df['only_date'] = df['date'].dt.date
-    df['only_date'] = pd.to_datetime(df['only_date'])
+    df['only_d'] = df['date'].dt.date
+    df['only_date'] = pd.to_datetime(df['only_d'])
     df['day'] = df['date'].dt.day
     df['day_name'] = df['date'].dt.day_name()
     df['hour'] = df['date'].dt.hour
